79142bcac202e966c6b5b1e9c47f26779ce91f11
mikeprussell <mikeprussell@outlook.com>
2021-09-15 21:02:27 -0500

    Template conversion completed

--- StoreFrontPowKit.UI.MVC/Controllers/HomeController.cs
+++ StoreFrontPowKit.UI.MVC/Controllers/HomeController.cs
@@ -1,4 +1,8 @@
-﻿using System.Web.Mvc;
+﻿using StoreFrontPowKit.UI.MVC.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Web.Configuration;
+using System.Web.Mvc;
 
 namespace StoreFrontPowKit.UI.MVC.Controllers
 {
@@ -11,20 +15,63 @@ namespace StoreFrontPowKit.UI.MVC.Controllers
         }
 
         [HttpGet]
-        [Authorize]
         public ActionResult About()
         {
-            ViewBag.Message = "Your app description page.";
+            return View();
+        }
 
+        [HttpGet]
+        public ActionResult AllProducts()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult IndividualProduct()
+        {
             return View();
         }
 
         [HttpGet]
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Contact(ContactViewModel cvm)
+        {
+            string body = $"Message received from PowKit. From {cvm.Name} - Subject {cvm.Subject}<br />Message:" +
+                $"{cvm.Message}";
+
+            string emailUser = WebConfigurationManager.AppSettings["EmailUser"];
+            string emailToAddress = WebConfigurationManager.AppSettings["EmailToAddress"];
+            string emailServer = WebConfigurationManager.AppSettings["EmailServer"];
+            string emailPassword = WebConfigurationManager.AppSettings["EmailPassword"];
+
+            MailMessage msg = new MailMessage(emailUser, emailToAddress, "Message from your contact form", body);
+
+            msg.IsBodyHtml = true;
+
+            SmtpClient client = new SmtpClient(emailServer);
+            client.Credentials = new NetworkCredential(emailUser, emailPassword);
+
+            try
+            {
+                client.Send(msg);
+                return View("EmailConfirmation", cvm);
+
+            }
+            catch (System.Exception ex)
+            {
+                ViewBag.ErrorMessage = "Oops. Something went wrong - try again or contact an administrator.";
+
+            }
+
+            return View();
+
+        }
+
     }
+
 }
---
+++ StoreFrontPowKit.UI.MVC/Models/ContactViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; //Added for validation
+
+namespace StoreFrontPowKit.UI.MVC.Models
+{
+    public class ContactViewModel
+    {
+        [Required(ErrorMessage = "* Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "* Email is required")]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "* Subject is required")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "* Message is required")]
+        [UIHint("MultilineText")]
+        public string Message { get; set; }
+
+    }
+}
