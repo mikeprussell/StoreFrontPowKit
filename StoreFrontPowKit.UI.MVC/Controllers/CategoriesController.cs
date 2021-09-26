5a0c4cf2934e0424d1bdedfc973ab6dddf758f48
mikeprussell <mikeprussell@outlook.com>
2021-09-25 22:44:49 -0500

    Ajax functionality for Category completed

--- StoreFrontPowKit.UI.MVC/Controllers/CategoriesController.cs
+++ StoreFrontPowKit.UI.MVC/Controllers/CategoriesController.cs
@@ -21,103 +21,165 @@ namespace StoreFrontPowKit.UI.MVC.Controllers
             return View(db.Categories.ToList());
         }
 
-        // GET: Categories/Details/5
-        [Authorize(Roles = "Admin, Employee, Operations Employee")]
-        public ActionResult Details(short? id)
+        #region Original Scaffolded, Non-Ajax CRUD Functionality
+        //// GET: Categories/Details/5
+        //[Authorize(Roles = "Admin, Employee, Operations Employee")]
+        //public ActionResult Details(short? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Category category = db.Categories.Find(id);
+        //    if (category == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(category);
+        //}
+
+        //// GET: Categories/Create
+        //[Authorize(Roles = "Admin, Operations Employee")]
+        //public ActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //// POST: Categories/Create
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Categories.Add(category);
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+
+        //    return View(category);
+        //}
+
+        //// GET: Categories/Edit/5
+        //[Authorize(Roles = "Admin, Operations Employee")]
+        //public ActionResult Edit(short? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Category category = db.Categories.Find(id);
+        //    if (category == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(category);
+        //}
+
+        //// POST: Categories/Edit/5
+        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(category).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(category);
+        //}
+
+        //// GET: Categories/Delete/5
+        //[Authorize(Roles = "Admin, Operations Employee")]
+        //public ActionResult Delete(short? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Category category = db.Categories.Find(id);
+        //    if (category == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(category);
+        //}
+
+        //// POST: Categories/Delete/5
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(short id)
+        //{
+        //    Category category = db.Categories.Find(id);
+        //    db.Categories.Remove(category);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing)
+        //    {
+        //        db.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        #endregion
+
+        // Delete
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult AjaxDelete(int id)
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
             Category category = db.Categories.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            return View(category);
-        }
 
-        // GET: Categories/Create
-        [Authorize(Roles = "Admin, Operations Employee")]
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Categories/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Categories.Add(category);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-
-            return View(category);
-        }
-
-        // GET: Categories/Edit/5
-        [Authorize(Roles = "Admin, Operations Employee")]
-        public ActionResult Edit(short? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Category category = db.Categories.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Categories/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(category).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(category);
-        }
-
-        // GET: Categories/Delete/5
-        [Authorize(Roles = "Admin, Operations Employee")]
-        public ActionResult Delete(short? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Category category = db.Categories.Find(id);
-            if (category == null)
-            {
-                return HttpNotFound();
-            }
-            return View(category);
-        }
-
-        // POST: Categories/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(short id)
-        {
-            Category category = db.Categories.Find(id);
             db.Categories.Remove(category);
             db.SaveChanges();
-            return RedirectToAction("Index");
+
+            string confirmMessage = $"Deleted category {category.CategoryName} from the database!";
+            return Json(new { id = id, message = confirmMessage });
+        }
+
+        [HttpGet]
+        public PartialViewResult CategoryDetails(int id)
+        {
+            Category category = db.Categories.Find(id);
+            return PartialView(category);
+
+        }
+
+        // Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult AjaxCreate(Category category)
+        {
+            db.Categories.Add(category);
+            db.SaveChanges();
+
+            return Json(category);
+
+        }
+
+        // Edit
+        [HttpGet]
+        public PartialViewResult CategoryEdit(int id)
+        {
+            Category category = db.Categories.Find(id);
+            return PartialView(category);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public JsonResult AjaxEdit(Category category)
+        {
+            db.Entry(category).State = EntityState.Modified;
+            db.SaveChanges();
+            return Json(category);
         }
 
         protected override void Dispose(bool disposing)
