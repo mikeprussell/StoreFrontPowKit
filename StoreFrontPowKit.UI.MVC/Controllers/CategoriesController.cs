e31b660b1067ba0b369647de195d84c44eb21526
mikeprussell <mikeprussell@outlook.com>
2021-10-10 10:21:39 -0500

    AJAX functionality completed

--- StoreFrontPowKit.UI.MVC/Controllers/CategoriesController.cs
+++ StoreFrontPowKit.UI.MVC/Controllers/CategoriesController.cs
@@ -22,113 +22,113 @@ namespace StoreFrontPowKit.UI.MVC.Controllers
         }
 
         #region Original Scaffolded, Non-Ajax CRUD Functionality
-        //// GET: Categories/Details/5
-        //[Authorize(Roles = "Admin, Employee, Operations Employee")]
-        //public ActionResult Details(short? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Category category = db.Categories.Find(id);
-        //    if (category == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(category);
-        //}
+        // GET: Categories/Details/5
+        [Authorize(Roles = "Admin, Employee, Operations Employee")]
+        public ActionResult Details(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
 
-        //// GET: Categories/Create
-        //[Authorize(Roles = "Admin, Operations Employee")]
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
+        // GET: Categories/Create
+        [Authorize(Roles = "Admin, Operations Employee")]
+        public ActionResult Create()
+        {
+            return View();
+        }
 
-        //// POST: Categories/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Category category)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Categories.Add(category);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
+        // POST: Categories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
 
-        //    return View(category);
-        //}
+            return View(category);
+        }
 
-        //// GET: Categories/Edit/5
-        //[Authorize(Roles = "Admin, Operations Employee")]
-        //public ActionResult Edit(short? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Category category = db.Categories.Find(id);
-        //    if (category == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(category);
-        //}
+        // GET: Categories/Edit/5
+        [Authorize(Roles = "Admin, Operations Employee")]
+        public ActionResult Edit(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
 
-        //// POST: Categories/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see https://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(category).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(category);
-        //}
+        // POST: Categories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CategoryID,CategoryName")] Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(category).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(category);
+        }
 
-        //// GET: Categories/Delete/5
-        //[Authorize(Roles = "Admin, Operations Employee")]
-        //public ActionResult Delete(short? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    Category category = db.Categories.Find(id);
-        //    if (category == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(category);
-        //}
+        // GET: Categories/Delete/5
+        [Authorize(Roles = "Admin, Operations Employee")]
+        public ActionResult Delete(short? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Category category = db.Categories.Find(id);
+            if (category == null)
+            {
+                return HttpNotFound();
+            }
+            return View(category);
+        }
 
-        //// POST: Categories/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(short id)
-        //{
-        //    Category category = db.Categories.Find(id);
-        //    db.Categories.Remove(category);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
+        // POST: Categories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(short id)
+        {
+            Category category = db.Categories.Find(id);
+            db.Categories.Remove(category);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
 
-        //protected override void Dispose(bool disposing)
-        //{
-        //    if (disposing)
-        //    {
-        //        db.Dispose();
-        //    }
-        //    base.Dispose(disposing);
-        //}
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
         #endregion
 
@@ -177,18 +177,9 @@ namespace StoreFrontPowKit.UI.MVC.Controllers
         [ValidateAntiForgeryToken]
         public JsonResult AjaxEdit(Category category)
         {
-            db.Entry(category).State = EntityState.Modified;
+            db.Categories.Add(category);
             db.SaveChanges();
             return Json(category);
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
